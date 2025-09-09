53c14676c90f0cfe734f27a0da469dd185909020
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-09-09 18:11:17 +0530

    ddfdf

--- vendor/forms.py
+++ vendor/forms.py
@@ -462,7 +462,6 @@ class PurchaseForm(forms.ModelForm):
 
         if user is not None:
             self.fields['vendor'].queryset = vendor_vendors.objects.filter(user=user)
-            self.fields['customer'].queryset = vendor_customers.objects.filter(user=user)
             self.fields['advance_bank'].queryset = vendor_bank.objects.filter(user=user)
 
 
