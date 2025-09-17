ea686db8931e2ea9438d8ce1f99631bdd388f7ac
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-09-17 19:36:13 +0530

    ddfdf

--- vendor/filters.py
+++ vendor/filters.py
@@ -12,3 +12,24 @@ class couponFilter(django_filters.FilterSet):
     class Meta:
         model = coupon
         exclude = ['image']  # ⛔ Exclude unsupported field
+
+
+
+
+class ProductFilter(django_filters.FilterSet):
+    # Extra handy filters
+    name = django_filters.CharFilter(lookup_expr="icontains")
+    brand_name = django_filters.CharFilter(lookup_expr="icontains")
+    color = django_filters.CharFilter(lookup_expr="icontains")
+    size = django_filters.CharFilter(lookup_expr="icontains")
+    description = django_filters.CharFilter(lookup_expr="icontains")
+
+    min_price = django_filters.NumberFilter(field_name="sales_price", lookup_expr="gte")
+    max_price = django_filters.NumberFilter(field_name="sales_price", lookup_expr="lte")
+
+    expiry_before = django_filters.DateFilter(field_name="expiry_date", lookup_expr="lte")
+    expiry_after = django_filters.DateFilter(field_name="expiry_date", lookup_expr="gte")
+
+    class Meta:
+        model = product
+        exclude = ["image", "gallery_images"]  
