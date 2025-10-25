c8c4ea4611c0b8e4223f92685836f9f2e5c6a6cb
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-10-25 23:02:43 +0530

    Ignore prod-only files

--- customer/models.py
+++ customer/models.py
@@ -63,8 +63,8 @@ class Address(models.Model):
     landmark = models.CharField(max_length=255, blank=True, null=True)
     town_city = models.CharField(max_length=100)
     state = models.CharField(max_length=100, choices=STATE_CHOICES)  # 🔽 dropdown
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=50, decimal_places=6)
+    longitude = models.DecimalField(max_digits=50, decimal_places=6)
 
     is_default = models.BooleanField(default=False)
     delivery_instructions = models.TextField(blank=True, null=True)
