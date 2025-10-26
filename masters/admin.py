5e8e2167898dd75107351bc2caa4b7c0822399f3
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-10-26 23:37:20 +0530

    Ignore prod-only files

--- masters/admin.py
+++ masters/admin.py
@@ -7,3 +7,6 @@ from .models import *
 
 admin.site.register(customer_address)
 admin.site.register(testimonials)
+admin.site.register(MainCategory)
+admin.site.register(product_category)
+admin.site.register(product_subcategory)
