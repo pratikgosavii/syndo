128cd5eba114540629d1e069dab00679b3989b65
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-12-23 23:34:12 +0530

    sdsd

--- masters/admin.py
+++ masters/admin.py
@@ -3,10 +3,15 @@ from .models import *
 
 # Register your models here.
 
-from .models import *
-
 admin.site.register(customer_address)
 admin.site.register(testimonials)
-admin.site.register(MainCategory)
+admin.site.register(expense_category)
+admin.site.register(size)
 admin.site.register(product_category)
+admin.site.register(MainCategory)
 admin.site.register(product_subcategory)
+admin.site.register(event)
+admin.site.register(company)
+admin.site.register(home_banner)
+admin.site.register(Pincode)
+admin.site.register(State)
